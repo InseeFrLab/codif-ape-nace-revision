521f43159e7f533bc733db82ffbb4cfd7040d430
ThomasFaria <thomas.faria@ensae.fr>
2025-07-11 14:02:40 +0000

    adjust list of models that needs specific configs to run on a single GPU

--- src/constants/llm.py
+++ src/constants/llm.py
@@ -2,24 +2,23 @@ MAX_NEW_TOKEN = 100
 TEMPERATURE = 0.01
 
 MODEL_TO_ARGS = {
-    "mistralai/Ministral-8B-Instruct-2410": {
-        "tokenizer_mode": "mistral",
-        "config_format": "mistral",
-        "load_format": "mistral",
-    },
-    "mistralai/Mistral-Small-3.1-24B-Instruct-2503": {
-        "tokenizer_mode": "mistral",
-        "config_format": "mistral",
-        "load_format": "mistral",
-    },
-    "Qwen/Qwen2.5-32B-Instruct": {
-        "max_model_len": 20000,
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B": {
+        "max_model_len": 25000,
         "gpu_memory_utilization": 0.95,
-        "enforce_eager": True,
+    },
+    "mistralai/Mistral-Small-3.2-24B-Instruct-2506": {
+        "tokenizer_mode": "mistral",
+        "config_format": "mistral",
+        "load_format": "mistral",
+        "max_model_len": 25000,
+        "gpu_memory_utilization": 0.95,
+    },
+    "Qwen/Qwen3-32B": {
+        "max_model_len": 25000,
+        "gpu_memory_utilization": 0.95,
     },
     "google/gemma-3-27b-it": {
-        "max_model_len": 20000,
+        "max_model_len": 25000,
         "gpu_memory_utilization": 0.95,
-        "enable_chunked_prefill": True,
     },
 }
