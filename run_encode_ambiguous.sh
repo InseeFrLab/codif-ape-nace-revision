820f5340dd1ed0beb81f0a21fe024792a1fd8b19
jpramil <julien.pramil@insee.fr>
2025-08-21 10:24:27 +0000

    start mapping errors analyzes

--- run_encode_ambiguous.sh
+++ run_encode_ambiguous.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-#Qwen/Qwen3-0.6B
+# Qwen/Qwen3-0.6B
 # export HF_HOME=/home/onyxia/work/.cache
-# export MODEL_NAME=Qwen/Qwen2-7B-Instruct
-# uv run huggingface-cli download $MODEL_NAME
+export MODEL_NAME=Qwen/Qwen2-7B-Instruct
+uv run huggingface-cli download $MODEL_NAME
 
 
-uv run src/encode_ambiguous_test.py \
+uv run src/encode_ambiguous.py \
     --strategy rag \
     --experiment_name NACE2025_DATASET \
     --collection_name embeddings_qwen_light \
     --llm_name Qwen/Qwen2-7B-Instruct \
-    --third 1
 
-uv run src/encode_ambiguous_test.py \
+uv run src/encode_ambiguous.py \
     --strategy rag \
     --experiment_name NACE2025_DATASET \
     --collection_name embeddings_qwen_semi_light \
     --llm_name Qwen/Qwen2-7B-Instruct \
-    --third 1
 
-uv run src/encode_ambiguous_test.py \
+uv run src/encode_ambiguous.py \
     --strategy rag \
     --experiment_name NACE2025_DATASET \
     --collection_name embeddings_qwen \
     --llm_name Qwen/Qwen2-7B-Instruct \
-    --third 1 \
