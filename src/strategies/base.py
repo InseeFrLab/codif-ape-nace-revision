04f978a9a7278278ac204cad01aeda67a78e64c2
jpramil <julien.pramil@insee.fr>
2025-08-25 14:33:25 +0000

    adapt prompts with cj labels

--- src/strategies/base.py
+++ src/strategies/base.py
@@ -98,8 +98,11 @@ class EncodeStrategy(ABC):
         if row.get("activ_sec_agri_et"):
             activity += f"\nPrécisions sur l'activité agricole : {row.get('activ_sec_agri_et').lower()}"
 
-        if row.get("activ_nat_lib_et"):
-            activity += f"\nAutre nature d'activité : {row.get('activ_nat_lib_et').lower()}"
+        if row.get("activ_nat_lib_et_1"):
+            activity += f"\nAutre nature d'activité : {row.get('activ_nat_lib_et_1').lower()}"
+
+        if row.get("lib_cj"):
+            activity += f"\nCatégorie juridique de l'établissement : {row.get('lib_cj').lower()}"
 
         return activity
 
