0105a995eef5e2b3aeee3ebe840790aac82aa142
jpramil <julien.pramil@insee.fr>
2025-08-29 15:59:31 +0000

    try with tomaarsen/Qwen3-Reranker-0.6B-seq-cls

--- poc_reranker.py
+++ poc_reranker.py
@@ -1,9 +1,9 @@
 from vllm import LLM
 from qdrant_client.http.models import NamedVector, SearchRequest
-data
-
+# data
+import pandas as pd
+data = pd.DataFrame({"libelle": ["boucher charcutier", "supermarché hard discount", "eleveur de chameau", "loueur de voiture", "restaurateur", "coiffeur", "vendeur en ligne", "artisan horloger", "agriculteur bio", "technicien informatique"]})
 activities, queries = strategy._prepare_queries(data)
-
 
 def _chunked(seq, size):
     """Split a list into chunks of given size."""
@@ -19,15 +19,26 @@ batch_size=2
 use_reranker=True
 
 if use_reranker:
-    reranker = LLM(
-            model="Qwen/Qwen3-Reranker-0.6B",
-            task="score",
-            hf_overrides={
-                "architectures": ["Qwen3ForSequenceClassification"],
-                "classifier_from_token": ["no", "yes"],
-                "is_original_qwen3_reranker": True,
-            },
-        )
+    # reranker = LLM(
+    #         model="Qwen/Qwen3-Reranker-0.6B",
+    #         task="score",
+    #         hf_overrides={
+    #             "architectures": ["Qwen3ForSequenceClassification"],
+    #             "classifier_from_token": ["no", "yes"],
+    #             "is_original_qwen3_reranker": True,
+    #         },
+    #     )
+    # reranker = LLM(
+    #     model="Qwen/Qwen3-Reranker-0.6B",
+    #     runner="pooling",
+    #     hf_overrides={
+    #         "architectures": ["Qwen3ForSequenceClassification"],
+    #         "classifier_from_token": ["no", "yes"],
+    #         "is_original_qwen3_reranker": True,
+    #     },
+    # )
+    reranker = LLM(model="tomaarsen/Qwen3-Reranker-0.6B-seq-cls", runner="pooling")
+
     document_template = "<Document>: {doc}{suffix}"
     suffix = "<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\n"
     query_template = "{prefix}<Instruct>: {instruction}\n<Query>: {query}\n"
@@ -49,24 +60,32 @@ for use_reranker in [True, False]:
 
     start_time = time.time()
     chunks = strategy._chunked(search_requests, batch_size)
+    # chunk = next(chunks)
     retrieved_docs = []
     for chunk in chunks:
         batch_results = strategy.db.client.search_batch(
             collection_name=strategy.collection_name,
             requests=chunk,
         )
-        retrieved_docs.append(batch_results)
+        retrieved_docs.extend(batch_results)
 
     results = []
+    # i=0
+    # retrieved_docs_single = retrieved_docs[i]
     for i, retrieved_docs_single in enumerate(retrieved_docs):
-        tmpl_retrieved_docs_single = [document_template.format(doc=doc.payload["page_content"], suffix=suffix) for doc in retrieved_docs_single]
-        query = [query_template.format(prefix=prefix, instruction=instruction, query=activities[i])]
-        outputs = reranker.score(query, tmpl_retrieved_docs_single)
-        scores = [output.outputs.score for output in outputs]
-        scored_docs = list(zip(retrieved_docs_single, scores))
-        scored_docs.sort(key=lambda x: x[1], reverse=True)
-        top_docs = [doc for doc, score in scored_docs[:5]]
-        results.append = top_docs
+        try:
+            tmpl_retrieved_docs_single = [document_template.format(doc=doc.payload["page_content"], suffix=suffix) for doc in retrieved_docs_single]
+            query = [query_template.format(prefix=prefix, instruction=instruction, query=activities[i])]
+            outputs = reranker.score(query, tmpl_retrieved_docs_single)
+            scores = [output.outputs.score for output in outputs]
+            scored_docs = list(zip(retrieved_docs_single, scores))
+            scored_docs.sort(key=lambda x: x[1], reverse=True)
+            top_docs = [doc for doc, score in scored_docs[:5]]
+            results.append(top_docs)
+        except Exception as e:
+            print(f"\n\nError in iteration {i}: {str(e)}=============\n\n==============\n\n")
+            raise
+
 
     end_time = time.time()
     print(f"Total time with use_reranker = {use_reranker}: {end_time - start_time:.2f} seconds")
@@ -105,9 +124,9 @@ len(chunk)
 
 
 
-results = strategy._search_qdrant(embeddings, top_k, batch_size, use_reranker)
+results = strategy._search_qdrant(embeddings, top_k=35, batch_size=2, use_reranker=False)
 
-results[0][0].
+results[0][0]
 
 
 
@@ -122,7 +141,7 @@ suffix = "<|im_end|>\n<|im_start|>assistant\n<think>\n\n</think>\n\n"
 
 query_template = "{prefix}<Instruct>: {instruction}\n<Query>: {query}\n"
 
-query = data["libelle"].loc[0]
+query = data["libelle"].loc[1]
 queries = [query_template.format(prefix=prefix, instruction=instruction, query=query)]
 len(queries)
 documents = [document_template.format(doc=doc.payload["page_content"], suffix=suffix) for doc in results[0]]
@@ -133,3 +152,32 @@ len(outputs)
 print("-" * 30)
 print([output.outputs.score for output in outputs])
 print("-" * 30)
+
+queries_save = queries.copy()
+documents_save=documents.copy()
+
+query
+tmpl_retrieved_docs_single
+
+queries_save == query
+documents_save == tmpl_retrieved_docs_single
+
+type(documents_save)
+type(tmpl_retrieved_docs_single)
+
+len(documents_save)
+len(tmpl_retrieved_docs_single)
+
+type(documents_save[0])
+type(tmpl_retrieved_docs_single[0])
+
+documents_save[0]
+
+for j in range(35):
+    print(f"\nDOCUMENT {j} : ===============\n")
+    tmpl_retrieved_docs_single[3]
+
+
+
+reranker.score(queries_save, documents_save)
+reranker.score(query, tmpl_retrieved_docs_single)
